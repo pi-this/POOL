794ae7c6acc9b9c71198409f2bb4d30ade3688b5
Wesley <114669697+pi-this@users.noreply.github.com>
2025-11-12 11:46:54 -0500

    Update operating.py

--- POOL/operating.py
+++ POOL/operating.py
@@ -1,10 +1,2 @@
-RunWhenSet = False
-CompileWhenSet = False
 def version():
     print( "version 1.0.0" )
-    
-def RunWhenSet():
-    RunWhenSet = True
-    
-def CompileWhenSet():
-    RunWhenSet = True
